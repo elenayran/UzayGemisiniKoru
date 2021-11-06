0edb5d806d2e8c2d506413cf6753f39ebaf0f313
elenayran <36763284+elenayran@users.noreply.github.com>
2021-11-06 23:11:25 +0300

    dusmanlarin hareketi

--- Assets/Scripts/DusmanlarinÇiktigiYer.cs
+++ Assets/Scripts/DusmanlarinÇiktigiYer.cs
@@ -7,9 +7,19 @@ public class DusmanlarinÇiktigiYer : MonoBehaviour
     public GameObject dusmanPrefabi;
     public float genislik;
     public float yukseklik;
+    public float speed;
+
+    public bool sagaHareket = true;
+    private float xmax;
+    private float xmin;
     // Start is called before the first frame update
     void Start()
     {
+        float ObjeIleKameraZsininFarki = transform.position.z - Camera.main.transform.position.z;
+        Vector3 kameraninSolTarafi = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, ObjeIleKameraZsininFarki));
+        Vector3 kameraninSagTarafi = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, ObjeIleKameraZsininFarki));
+        xmin = kameraninSolTarafi.x;
+        xmax = kameraninSagTarafi.x;
         foreach (Transform cocuk in transform)
         {
             GameObject enemy = Instantiate(dusmanPrefabi,cocuk.transform.position, Quaternion.identity) as GameObject;
@@ -25,6 +35,23 @@ public class DusmanlarinÇiktigiYer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (sagaHareket)
+        {
+            // transform.position += new Vector3(speed * Time.deltaTime, 0);
+            transform.position += Vector3.right *speed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        float sagSinir = transform.position.x + genislik / 2;
+        float solSinir = transform.position.x - genislik / 2;
+
+        if (sagSinir > xmax || solSinir < xmin )
+        {
+
+            sagaHareket = !sagaHareket;
+        }
         
     }
 }
